d674a2ab5cc87f82b54f8e0e26ad21dcd5a1f277
Anton Isakov <anton.isakov@stud.techin.lt>
2024-06-22 22:09:37 +0300

    fake data generation added

---
+++ bookRecommendationApp/src/main/java/com/techin/bookRecommendationApp/datageneration/DataGenerator.java
@@ -0,0 +1,61 @@
+package com.techin.bookRecommendationApp.datageneration;
+
+import com.github.javafaker.Faker;
+import com.techin.bookRecommendationApp.Enums.Role;
+import com.techin.bookRecommendationApp.entity.Book;
+import com.techin.bookRecommendationApp.entity.Category;
+import com.techin.bookRecommendationApp.entity.User;
+import com.techin.bookRecommendationApp.repository.BookRepository;
+import com.techin.bookRecommendationApp.repository.CategoryRepository;
+import com.techin.bookRecommendationApp.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataGenerator {
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
+    private final CategoryRepository categoryRepository;
+
+    @PostConstruct
+    public void generateData(){
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 10; i++){
+            User user = new User();
+            user.setFirstName(faker.name().firstName());
+            user.setLastName(faker.name().lastName());
+            user.setRole(Role.USER);
+            user.setEmail(faker.internet().emailAddress("gmail"));
+            user.setPassword("1234");
+
+            userRepository.save(user);
+        }
+
+        for (int i = 0; i < 10; i++){
+            Book book = new Book();
+            book.setAuthor(faker.book().author());
+            book.setName(faker.book().title());
+            book.setIsbn(faker.book().genre());
+            book.setDescription(faker.lorem().characters(50,250));
+            book.setPagesCount(faker.number().numberBetween(100,400));
+
+            bookRepository.save(book);
+        }
+
+        for (int i = 0; i < 10; i++){
+            Category category = new Category();
+
+            category.setName(faker.commerce().productName());
+            category.addBook(bookRepository.findAll().get(faker.number().numberBetween(0,9)));
+            category.addBook(bookRepository.findAll().get(faker.number().numberBetween(0,9)));
+
+            categoryRepository.save(category);
+        }
+
+
+    }
+
+}
